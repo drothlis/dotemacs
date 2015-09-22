16e8a95b8482c9acf2bc684b8d5e203016034280
David Röthlisberger <david@rothlis.net>
2015-09-22 14:19:02 +0100

    Provisional email configuration
    I never ended up using this. Committing it for documentation purposes in
    case I come back to it.

--- init.el
+++ init.el
@@ -516,6 +516,29 @@ word boundaries) in text-mode-hook."
     (t (error "my-frame-move: Invalid position code")))))
 
 
+;;; Email
+
+(require 'gnus)
+(setq nnml-directory "~/Documents/gmail")
+(setq message-directory "~/Documents/gmail")
+(setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\”]\”[#’()]")
+
+(setq gnus-select-method
+      '(nnimap "gmail"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port 993)
+               (nnimap-stream ssl)))
+(setq starttls-use-gnutls t)
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "drothlis@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
+
+(setq gnus-large-newsgroup nil)
+
+
 ;;; OS X
 
 (when (eq system-type 'darwin)
